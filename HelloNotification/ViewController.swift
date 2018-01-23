17de519c7c3f39796315b222304363bb18c719c1
freyahu131 <freyahu131@gmail.com>
2018-01-24 06:20:56 +0800

    notification 鍵盤會升起下降

--- HelloNotification/ViewController.swift
+++ HelloNotification/ViewController.swift
@@ -8,18 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    
+    @IBOutlet var bottomConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //註冊「我在意鍵盤即將升起的通知」
+        NotificationCenter.default.addObserver(self, selector: #selector (moveTextFieldUP(aNotification: )), name: .UIKeyboardWillShow, object: nil)
     }
-
+    
+    //當鍵盤即將升起時會執行的方法
+    @objc func moveTextFieldUP(aNotification: Notification){
+        //取出詳情
+        let info = aNotification.userInfo
+        //取出尺寸
+        let sizeValue = info![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        //將尺寸值轉換為 CGSize
+        let size = sizeValue.cgRectValue.size
+        //拿出高度
+        let height = size.height
+        //把底部的距離改為0
+        bottomConstraint.constant = -height
+        //利用動畫增加延遲感
+        UIView.animate(withDuration: 25) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        bottomConstraint.constant = 0
+        return true
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
 }
+
+
+
 
